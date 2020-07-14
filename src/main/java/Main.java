720882d463cfe9845815669bc08218c81e471168
Panos <panos_vk@hotmail.com>
2020-07-14 12:56:58 +0300

    allagi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+
+
 public static  void main { String[] args){
     System.out.println("Hello Panos");
         System.out.println("Hello Word");
@@ -6,5 +8,7 @@ public static  void main { String[] args){
 
         System.out.println("Hello1 Word1");
 
+        System.out.println("allagi!!");
 
         }
+
