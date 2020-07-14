1cabfca60c5506a2fe02d85491e774c36bf3d488
Panos <panos_vk@hotmail.com>
2020-07-14 12:45:37 +0300

    play

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
-public static void main { String[] args){
+public static  void main { String[] args){
     System.out.println("Hello Panos");
         System.out.println("Hello Word");
 
         System.out.println("Hello1 Word1");
 
-}
+        System.out.println("Hello1 Word1");
+
+
+        }
